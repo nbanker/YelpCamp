1e141423580496a4464c22b017626ec3fdd2c0b1
bank <originalbanker@gmail.com>
2020-07-07 07:53:17 +0000

    add atlasdb connection to app.js

--- app.js
+++ app.js
@@ -17,7 +17,17 @@ var commentRoutes    = require("./routes/comments"),
 	indexRoutes      = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v12", {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp_v12", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://bank:bankpassword@yelpcamp.taapa.mongodb.net/YelpCamp?retryWrites=true&w=majority", {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+}).then(() => {
+	console.log("Connected to DB!");
+}).catch(err => {
+	console.log("ERROR", err.message);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
